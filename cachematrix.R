e5be4565a94891d90132e19db038cb3bb1f7c0c8
ZolotukhinAS <zolotukhin.a.s@gmail.com>
2016-01-25 14:03:42 +0100

    2nd Program assignment initial implementation

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # storage for cached inversed matrix
+        m_i <- NULL
+        
+        # constructor
+        set <- function(y = matrix()){
+                x <<- y
+                m_i <<- NULL
+        }
+        
+        # return inital matrix itself
+        get <- function () x
+        
+        # set cached inverse value
+        setInverse <- function(inv) m_i <- inv
+        
+        # get cached inverse value
+        getInverse <- function() m_i
+        
+        #wrapper for the object
+        list(get = get, set = set,
+             getInverse = getInverse, setInverse = setInverse)
 }
 
 
